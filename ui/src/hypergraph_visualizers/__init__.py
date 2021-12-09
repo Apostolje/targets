2fb8fe879ce5e6fa08b373c253bd56eb4113254a
Elisey Nigodin <apostolje@gmail.com>
2021-12-10 01:47:49 +0300

    fix circular import

--- ui/src/hypergraph_visualizers/__init__.py
+++ ui/src/hypergraph_visualizers/__init__.py
@@ -3,10 +3,10 @@ from hypernetx import Hypergraph
 from matplotlib.axes import Axes
 
 from algorithms import Task, Solution
-from ui.src.hypergraph_visualizers.classic import Classic
 from ui.src.hypergraph_visualizers.utils import create_color_mapping
 from ui.src.hypergraph_visualizers.hypergraph_visualizer import HypergraphVisualizer
 from ui.src.hypergraph_visualizers.koenig import Koenig
+from ui.src.hypergraph_visualizers.classic import Classic
 
 # список всех доступных способов визуализации гиперграфа
 visualizers: list[Type[HypergraphVisualizer]] = [
